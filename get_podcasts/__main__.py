0273bdb086e79377f323a6ba56ad52fa6ecaa6ac
Jesse Butcher <boweeb@gmail.com>
2019-01-05 15:16:03 -0500

    Convert script to package

---
+++ get_podcasts/__main__.py
@@ -0,0 +1,7 @@
+"""get_podcasts.__main__: executed when get_podcasts directory is called as script."""
+
+
+from .get_podcasts import main
+
+
+main()
---
+++ get_podcasts_runner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 ts=8 et sw=4 sts=4 smarttab autoindent:
+# -*- coding: utf-8 -*-
+
+
+"""Dev Runner."""
+
+
+from get_podcasts.get_podcasts import main
+
+
+if __name__ == '__main__':
+    main()
